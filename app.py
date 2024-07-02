587b53e5d4fa75ca58effd080e98bcea406b5099
Shlok-21 <shlokshivkar21@gmail.com>
2024-07-02 17:01:15 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os
+from PIL import Image
+import pdf2image
+import google.generativeai as genai
+
+genai.configure(api_key = os.getenv('GOOGLE_API_KEY'))
+
+
